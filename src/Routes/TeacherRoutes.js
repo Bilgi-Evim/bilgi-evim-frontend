a848271cb52408c09b1650431a9a238a248273fb
Efe Eroğlu <210541019@firat.edu.tr>
2024-10-29 01:02:23 +0300

    update

--- src/Routes/TeacherRoutes.js
+++ src/Routes/TeacherRoutes.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
+// src/Routes/TeacherRoutes.js
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import TeacherDashboard from "../Components/Teacher/TeacherDashboard";
-import TeacherCourses from "../Components/Teacher/TeacherCourses"
+import TeacherCourses from "../Components/Teacher/TeacherCourses";
 import TeacherLogin from '../Components/Teacher/TeacherLogin';
 import StudentList from '../Components/Teacher/StudentList';
 import TeacherMessages from '../Components/Teacher/TeacherMessages';
 import Reports from '../Components/Teacher/Reports';
 import Settings from '../Components/Teacher/Settings';
+import PrivateRoute from './PrivateRoute';
 
-const TeacherRoutes = () => {
-  return (
-    <Routes>
-        <Route path='/' element={<Navigate to="/teacher/dashboard"/>}/>
-        <Route path="/login" element={<TeacherLogin />}/>
-        <Route path="/dashboard" element={<TeacherDashboard />}/>
-        <Route path="/courses" element={<TeacherCourses />}/>
-        <Route path="/students-list" element={<StudentList />}/>
-        <Route path="/tutor" element={<StudentList />}/>
-        <Route path="/messages" element={<TeacherMessages />}/>
-        <Route path="/reports" element={<Reports />}/>
-        <Route path="/settings" element={<Settings />}/>
-    </Routes>
-)
-}
+const TeacherRoutes = () => (
+  <Routes>
+    <Route path="/login" element={<TeacherLogin />} />
+    <Route element={<PrivateRoute roleRequired="teacher" />}>
+      <Route path="/dashboard" element={<TeacherDashboard />} />
+      <Route path="/courses" element={<TeacherCourses />} />
+      <Route path="/students-list" element={<StudentList />} />
+      <Route path="/messages" element={<TeacherMessages />} />
+      <Route path="/reports" element={<Reports />} />
+      <Route path="/settings" element={<Settings />} />
+    </Route>
+  </Routes>
+);
 
-export default TeacherRoutes
+export default TeacherRoutes;
